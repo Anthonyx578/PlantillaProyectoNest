c1f55144310e8a90bd229da86df5c46cc314befb
Anthonyx578 <antrogartme2002@gmail.com>
2025-03-07 18:35:32 -0500

    Añadido de class-transformer y class-validator para poder validar la entrada de datos.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/ExceptionFilter/HttpExceptionFilter';
+import { PipesConsumer } from '@nestjs/core/pipes';
 
 async function bootstrap() {
   const logger = new Logger('App');
 
-
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter());
-
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Solo permite cosas que esten validadas en los dts con el class validator
+      whitelist: true,
+      //Salta una excepcion cuando encuentra algo o existe algo de mas en el body de la peticion
+      forbidNonWhitelisted: true,
+      //Transforma lo que viene en la peticion en instancia de clase directamente
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('')
@@ -28,8 +43,7 @@ async function bootstrap() {
     },
   });
 
-
-  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(Number(process.env.Port));
   logger.log(`Servicio Iniciado en http://localhost:${process.env.Port}`);
